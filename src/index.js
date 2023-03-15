d7d9793e1f47930db3835d9dd6e79c54aedea81f
AndrewKobzar <andwkobzar@gmail.com>
2023-03-15 18:15:46 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-// import './css/styles.css';
-import './css/style.css'
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
